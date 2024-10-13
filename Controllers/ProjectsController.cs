a78e999f1cb14548ccfbc518186cecda5c0998ea
Mohammed <mohammedlilosoci@gmail.com>
2024-10-13 17:40:47 +0300

    Solve Task 02

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,51 @@
+﻿using AonFreelancing.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AonFreelancing.Controllers;
+
+[ApiController]
+[Route("api/v1/projects")]
+public class ProjectsController : ControllerBase
+{
+    static readonly List<Project> _projects = [];
+
+    [HttpGet]
+    public IActionResult GetProjects()
+    {
+        return Ok(_projects);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetProject(int id)
+    {
+        Project? project = _projects.Find(x => x.Id == id);
+
+        if (project == null)
+            return NotFound();
+
+        return Ok(project);
+    }
+
+    [HttpPost]
+    public IActionResult CreateProject([FromBody] Project project) {
+        if (project == null)
+            return BadRequest();
+
+        _projects.Add(project);
+        return CreatedAtAction(nameof(GetProject), new { id = project.Id}, _projects);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteProject(int id)
+    {
+        Project? project = _projects.Find(x => x.Id == id);
+        if (project == null)
+            return NotFound();
+
+        _projects.Remove(project);
+
+        //return Ok($"deleted {project}");
+        return NoContent();
+    }
+
+}
---
+++ Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml.Linq;
+
+namespace AonFreelancing.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Project(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+        public override string ToString()
+        {
+            return $"Project: Id={Id}, Title={Title}";
+        }
+    }
+}
